efdbb3d721ccc30d2d7ec5e92e5986c5ce769da3
Jacob <yawjacob@yahoo.com>
2021-09-24 20:11:16 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(freeSample){
+  let depth
+    if (freeSample <= 400) {
+      return 'This one is on me!'
+    }
+    
+    else if (freeSample < 2500) {
+      return 'I will gladly take your thirty bucks.'
+    }
+
+    else if (freeSample >= 2500) {
+      return 'No can do.'
+    }
+  }
+
+function ternaryCheckCity(city){
+  let destination
+  if (city === 'NYC') {
+    return 'Ok, sounds good.'
+  }
+  else {
+    return 'No go.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  let response;
+  switch (tip) {
+    case 'generous':
+        response = 'Thank you so much.';
+        break;
+    case 'not as generous':
+        response = 'Thank you.';
+        break;
+    default:
+        response = 'Bye.';
+        break;
+  }
+
+  return response
 }
